886a71e10aaeb3a3dd908fdb8bd7c8d0e5a2b9ec
Alexandre VIGOUREUX <alexandre.vigoureux@epitech.eu>
2023-03-29 16:55:50 +0200

    [add] bot logging

--- discord.js
+++ discord.js
@@ -1,2 +1,12 @@
-require('dotenv').config()
-console.log(process.env.BOT_TOKEN)
+require('dotenv').config();
+console.log(process.env.BOT_TOKEN);
+
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.BOT_TOKEN);
